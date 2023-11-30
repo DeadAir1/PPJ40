d9b3c7b48fc403908bc3816c89e2dac84e90cc51
DeadAir1 <s27565@pjwstk.edu.pl>
2023-11-30 17:07:55 +0100

    PPJ_c13

--- src/PPJ_c13/Main.java
+++ src/PPJ_c13/Main.java
@@ -1,8 +1,45 @@
 package PPJ_c13;
 
+import javax.swing.*;
 import java.util.Arrays;
 
 public class Main {
+    static void printMatrix(int arr[][]){
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <arr[i].length ; j++) {
+                if(arr[i][j]<10 && arr[i][j]>=0)
+                    System.out.print( "  " + arr[i][j]);
+                else  System.out.print(" " + arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    static void show(int a){
+        System.out.println(a);
+    }
+    static void modifyValue(int wrt){
+        show(wrt);
+        wrt*=5;
+        show(wrt);
+    }
+    static int findMax(int a,int b,int c){
+        if(a>=b && a>=c) return a;
+        if(b>=c && b>=a) return b;
+    return c;
+    }
+    static int[]func(int a[],int b[],int wrt){
+        int arr[]=new int[Math.min(a.length, b.length)];;
+        if (wrt<0){
+            for (int i = 0; i <arr.length ; i++) {
+                arr[i]=b[i]+a[i];
+            }
+        }
+        else if(wrt>0){
+
+        }
+
+
+    return arr;}
     public static void main(String[] args) {
         //Zad 1
        /* int arr[][]={
@@ -27,15 +64,40 @@ public class Main {
 
 
         //Zad 2
-        int[][] arr = {
-                {1,2,3,4,5,6},
-                {2,3,4,5,6,7},
-                {3,4,5,6,7,8},
-                {4,5,6,7,8,9}
+        /*int[][] arr = {
+                {1,4,3,4,5,3},
+                {2,2,4,5,2,7},
+                {7,4,5,6,7,8},
+                {9,5,6,7,8,9}
         };
-        int counter=0;
-        int [][] arr1=new int[arr.length][];
+        int arr1[][]=new int[arr.length][];*/
 
+
+        //Zad 3
+        /*int a=9;
+        show(a);*/
+
+        //Zad 4
+       /* int wrt=5;
+        show(wrt);
+        modifyValue(wrt);
+        show(wrt);*/
+
+
+        //Zad 5
+       /* int a=3,b=5,c=7;
+        System.out.println(findMax(a,b,b));*/
+
+
+        //Zad 6
+        int wrt=10;
+        int a[]=new int[10];
+        int b[]=new int[10];
+        for (int i = 0; i <a.length ; i++) {
+            a[i]=(int)(Math.random()*10+1);
+            b[i]=(int)(Math.random()*10+1);
+        }
 
     }
 }
+
