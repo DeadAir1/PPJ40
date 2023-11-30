788e8d8400c94f7585c580058538bd7c35cfd267
Oleksandr <s27565@pjwstk.edu.pl>
2023-11-30 21:03:40 +0100

    PRIVATE

--- src/PPJ_c13/Main.java
+++ src/PPJ_c13/Main.java
@@ -14,32 +14,45 @@ public class Main {
             System.out.println();
         }
     }
+    //FUNKCJA DO ZADANIA 3
     static void show(int a){
         System.out.println(a);
     }
+    //FUNKCJA DO ZADANIA 4
     static void modifyValue(int wrt){
         show(wrt);
         wrt*=5;
         show(wrt);
     }
+    //FUNKCJA DO ZADANIA 5
     static int findMax(int a,int b,int c){
         if(a>=b && a>=c) return a;
         if(b>=c && b>=a) return b;
     return c;
     }
-    static int[]func(int a[],int b[],int wrt){
-        int arr[]=new int[Math.min(a.length, b.length)];;
+    //FUNKCJA DO ZADANIA 6
+    static int[] func(int a[],int b[],int wrt){
+        int arr[]=null;
         if (wrt<0){
+            arr=new int[Math.min(a.length, b.length)];
             for (int i = 0; i <arr.length ; i++) {
                 arr[i]=b[i]+a[i];
             }
         }
         else if(wrt>0){
-
+            boolean aLonger= a.length > b.length;
+            arr=new int[aLonger ? a.length-b.length : b.length-a.length];
+            for (int i = 0; i <arr.length ; i++) {
+                if(aLonger){
+                    arr[i]=a[i+b.length];
+                }
+                else{ arr[i]=b[i+a.length];
+                }
+            }
         }
 
-
-    return arr;}
+             return arr;
+    }
     public static void main(String[] args) {
         //Zad 1
        /* int arr[][]={
@@ -64,13 +77,44 @@ public class Main {
 
 
         //Zad 2
-        /*int[][] arr = {
+   /*     int[][] arr = {
                 {1,4,3,4,5,3},
                 {2,2,4,5,2,7},
                 {7,4,5,6,7,8},
                 {9,5,6,7,8,9}
         };
-        int arr1[][]=new int[arr.length][];*/
+        int tab[]=new int[arr[0].length];
+        int arr1[][]=new int[arr.length][];
+        int index=0;
+        boolean jest=false;
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <arr[0].length ; j++) {
+                for (int k = 0; k < tab.length; k++) {
+                    if(tab[k]==arr[i][j]) {
+                        jest=true;
+                        break;
+                    }
+                }
+                if(!jest){
+                    tab[index]=arr[i][j];
+                    index++;
+                }
+                jest=false;
+
+            }
+            index=0;
+            int a=0;
+            while(tab[a]!=0){
+                a++;
+            }
+            arr1[i]=new int[a];
+            for (int j = 0; j <arr1[i].length ; j++) {
+                arr1[i][j]=tab[j];
+                tab[j]=0;
+            }
+            a=0;
+        }
+        printMatrix(arr1);*/
 
 
         //Zad 3
@@ -90,13 +134,18 @@ public class Main {
 
 
         //Zad 6
-        int wrt=10;
-        int a[]=new int[10];
+        int wrt=-10;
+        int a[]=new int[9];
         int b[]=new int[10];
         for (int i = 0; i <a.length ; i++) {
             a[i]=(int)(Math.random()*10+1);
             b[i]=(int)(Math.random()*10+1);
         }
+        b[9]=12;
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+
+        System.out.println(Arrays.toString(func(a,b,wrt)));
 
     }
 }
