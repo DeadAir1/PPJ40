eccbfabeaef8566f4e89c33a4f3cabef25530b08
DeadAir1 <s27565@pjwstk.edu.pl>
2023-12-30 12:56:32 +0100

    NotePad

--- src/PRIVATE/Notepad/NotepadApp.java
+++ src/PRIVATE/Notepad/NotepadApp.java
@@ -1,12 +1,27 @@
 package PRIVATE.Notepad;
 
+import java.util.Scanner;
+
 public class NotepadApp {
     public static void main(String[] args) {
-        NoteManager manager=new NoteManager();
-       Note note=manager.create("file");
-       System.out.println(note.getCreationDate());
-        FileManager.write(note);
-        FileManager.read(note);
-        manager.review(note);
+        System.out.println("Hi you open tour notepad\n" +
+                "Here you have 5 options: \n" +
+                "1.create\n" +
+                "2.delete\n" +
+                "3.review\n" +
+                "4.write\n" +
+                "5.read");
+        System.out.print("Please type what you wanna do or type a number,if you want to exit just type it :");
+        Scanner in=new Scanner(System.in);
+        String command="";
+        String [] commands={"create","delete","review","write","read"};
+        while((command= in.next()).equals("exit")){
+            switch (command ){
+
+            }
+        }
+
+
+
     }
 }
