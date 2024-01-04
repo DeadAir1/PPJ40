ec49f61b94acf42b86c64da0aefc8775f343e124
DeadAir1 <s27565@pjwstk.edu.pl>
2024-01-04 22:08:33 +0100

    NotePad

--- src/PPJ_17/SortedSLL.java
+++ src/PPJ_17/SortedSLL.java
@@ -6,27 +6,35 @@ public class SortedSLL {
             SortedSLL(){
                 this.head=null;
             }
-
             void addSorted(int data){
                 if(head==null || data<=head.data){
                     head=new Node(data,head);
                 }else{
                     Node tmp=head;
-                    while(tmp.next!=null || data>=tmp.next.data ) {
-
-
+                    while(tmp.next!=null && data>=tmp.data){
+                        tmp=tmp.next;
+                    }
+                    if(data>=tmp.data){
+                    tmp.next=new Node(data,null);
+                    }else{
+                        tmp=new Node(data,tmp);
+                        Node tmp1=head;
+                        while(tmp1.next.data<=data){
+                            tmp1=tmp1.next;
+                        }
+                        tmp1.next=tmp;
                     }
 
-                }
 
+                }
 
             }
             void show(){
                 Node tmp=head;
-                while(tmp.next!=null){
-                    System.out.println(tmp.data);
-                    tmp=tmp.next;
-                }
+               while(tmp!=null){
+                   System.out.println(tmp.data);
+                   tmp=tmp.next;
+               }
             }
 
 }
